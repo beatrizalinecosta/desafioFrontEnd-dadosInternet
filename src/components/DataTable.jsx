726ac7cdd54b0902419cc9ab98eca7461147dc9a
Beatriz Costa <beatrizcosta@Mac.local>
2025-12-03 15:36:58 -0300

    Detalhes design

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -55,13 +55,25 @@ export default function DataTable({ data }) {
     <div className="bg-gray-100 dark:bg-[#212529] p-4 rounded shadow">
       {/* CABEÇALHO DA SEÇÃO */}
       <div className="flex justify-between items-center mb-3">
-        <h2 className="text-xl font-semibold dark:text-white">Tabela Interativa</h2>
-        <button
-          onClick={() => downloadCSV(filtered)}
-          className="px-3 py-1 rounded border bg-[#DA4167] text-white"
-        >
-          Exportar CSV
-        </button>
+        <div className="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M5.616 20q-.691 0-1.153-.462T4 18.384V5.616q0-.691.463-1.153T5.616 4h12.769q.69 0 1.153.463T20 5.616v12.769q0 .69-.462 1.153T18.384 20zm5.884-5.596H5v3.98q0 .27.173.443t.443.173H11.5zm1 0V19h5.885q.269 0 .442-.173t.173-.442v-3.981zm-1-1V8.769H5v4.635zm1 0H19V8.769h-6.5zM5 7.769h14V5.615q0-.269-.173-.442T18.385 5H5.615q-.269 0-.442.173T5 5.616z"></path></svg>
+            <h2 className="text-xl font-semibold dark:text-white">Tabela Interativa</h2>
+        </div>
+        <div className="flex">
+            
+            <button
+            onClick={() => downloadCSV(filtered)}
+            className="px-3 py-1 rounded border bg-[#DA4167] text-white flex"
+            >
+            Exportar 
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                    <g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={1.5}>
+                        <path d="M7.5 17.22C7.445 16.03 6.622 16 5.505 16c-1.72 0-2.005.406-2.005 2v2c0 1.594.285 2 2.005 2c1.117 0 1.94-.03 1.995-1.22m13-4.78l-1.777 4.695c-.33.87-.494 1.305-.755 1.305c-.26 0-.426-.435-.755-1.305L15.436 16m-2.56 0h-1.18c-.473 0-.709 0-.895.076c-.634.26-.625.869-.625 1.424s-.009 1.165.625 1.424c.186.076.422.076.894.076s.708 0 .894.076c.634.26.625.869.625 1.424s.009 1.165-.625 1.424c-.186.076-.422.076-.894.076H10.41"></path>
+                        <path strokeLinejoin="round" d="M20 13v-2.343c0-.818 0-1.226-.152-1.594c-.152-.367-.441-.657-1.02-1.235l-4.736-4.736c-.499-.499-.748-.748-1.058-.896a2 2 0 0 0-.197-.082C12.514 2 12.161 2 11.456 2c-3.245 0-4.868 0-5.967.886a4 4 0 0 0-.603.603C4 4.59 4 6.211 4 9.456V13m9-10.5V3c0 2.828 0 4.243.879 5.121C14.757 9 16.172 9 19 9h.5"></path>
+                    </g>
+                </svg>
+            </button>
+        </div>
       </div>
 
       {/* FILTROS INTERNOS TABELA  */}
@@ -149,8 +161,11 @@ export default function DataTable({ data }) {
         <button
           disabled={page === 0}
           onClick={() => setPage((p) => p - 1)}
-          className="px-3 py-1 border rounded text-white bg-[#145C9E]"
+          className="px-3 py-1 border rounded text-white bg-[#145C9E] flex"
         >
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M10 21.308L.692 12L10 2.692l1.064 1.064L2.819 12l8.244 8.244z"></path>
+        </svg>
           Anterior
         </button>
 
@@ -161,9 +176,12 @@ export default function DataTable({ data }) {
         <button
           disabled={page === pageCount - 1}
           onClick={() => setPage((p) => p + 1)}
-          className="px-3 py-1 border rounded text-white bg-[#145C9E]"
+          className="px-3 py-1 border rounded flex text-white bg-[#145C9E]"
         >
           Próxima
+          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M15.187 12L7.47 4.285q-.221-.221-.218-.532q.003-.31.224-.532Q7.698 3 8.009 3q.31 0 .532.221l7.636 7.643q.242.242.354.54t.111.596t-.111.596t-.354.54L8.535 20.78q-.222.221-.53.218q-.307-.003-.528-.224t-.221-.532t.221-.531z"></path>
+        </svg>
         </button>
       </div>
     </div>
